c859e3932a542902da93b103cbcdd0510834c4a6
Sreeharisnair006 <sreeharisreekumarnair2028@cs.sjcetpalai.ac.in>
2025-03-17 09:55:42 +0530

    Add files via upload

--- matadd.c
+++ matadd.c
@@ -1,61 +1,59 @@
 /*
 Name: Sreehari Sreekumar Nair
-Date: 07 February 2025
-Purpose: Program to input two 2D arrays, perform matrix addition and print the resulting matrix.
+Date: March 17, 2025
+Purpose: Program that adds two matrices.
 */
-
 #include <stdio.h>
+void readMat(int[][10],int,int);
+void addMat(int[][10],int[][10],int[][10],int,int);
+void printMat(int[][10],int,int);
 int main() {
-  int row,col,i,j;
-  printf("Enter the number of rows and columns for the matrices: ");
+  int row,col;
+  printf("Enter the amount of rows and columns: ");
   scanf("%d%d",&row,&col);
   
-  int matrix1[row][col];
-  printf("\nEnter the elements of matrix 1: \n");
-  for(int i=0;i<row;i++){
-    for(int j=0;j<col;j++){
-      scanf("%d",&matrix1[i][j]);
-    }
-  }
+  int mat1[row][10];
+  printf("Enter the elements of the first matrix: ");
+  readMat(mat1,row,col);
   
-  int matrix2[row][col];
-  printf("\nEnter the elements of matrix 2: \n");
-  for(int i=0;i<row;i++){
-    for(int j=0;j<col;j++){
-      scanf("%d",&matrix2[i][j]);
-    }
-  }
+  int mat2[row][10];
+  printf("Enter the elements of the second matrix: ");
+  readMat(mat2,row,col);
   
-  
-  printf("\n First matrix:\n");
-  
-  for(i=0;i<row;i++){
-    for(j=0;j<col;j++){
-      printf("%d\t",matrix1[i][j]);
-      }
-    printf("\n");
-  }
-  
-  
-  
-  printf("\n Second matrix:\n");
-  
-  for(i=0;i<row;i++){
-    for(j=0;j<col;j++){
-      printf("%d\t",matrix2[i][j]);
-      }
-    printf("\n");
-  }
-  
-  int matrix3[i][j];
-  printf("\n Resultant matrix:\n");
-  for(i=0;i<row;i++){
-    for(j=0;j<col;j++){
-      matrix3[i][j] = matrix1[i][j] + matrix2[i][j];
-      printf("%d\t",matrix3[i][j]);
-      }
-    printf("\n");
-  }
-    
+  int mat3[row][10];
+  printf("Resulting matrix:\n");
+  addMat(mat1,mat2,mat3,row,col);
+  printMat(mat3,row,col);
   return 0;
 }
+
+void readMat(int mat[][10],int row,int col){
+  for(int i=0;i<row;i++){
+    for(int j=0;j<col;j++){
+      scanf("%d",&mat[i][j]);
+    }
+  }
+}
+
+void addMat(int mat1[][10],int mat2[][10],int mat3[][10],int row,int col){
+  for(int i=0;i<row;i++){
+    for(int j=0;j<col;j++){
+        mat3[i][j] = mat1[i][j] + mat2[i][j];
+    }
+  }
+}
+
+void printMat(int mat3[][10],int row,int col){
+  for(int i=0;i<row;i++){
+    for(int j=0;j<col;j++){
+      printf("%d\t",mat3[i][j]);
+    }
+    printf("\n");
+  }
+}
+  
+
+  
+  
+  
+  
